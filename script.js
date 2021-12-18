419cddcad81ba2467f12482b00cb584b3f20f4fa
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-12-18 23:47:25 +0530

    marginLeft

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function start() {
     var d = document.createElement("div");
     d.className = "d";
+
+    d.style.marginLeft = 500 + "px";
+
     var b = document.getElementById("b");
     b.appendChild(d);
 }
