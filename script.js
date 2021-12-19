9bbc984c10a2969316e06846e5733ba1bbc61b15
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-12-19 23:47:23 +0530

    x

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function start() {
     var d = document.createElement("div");
     d.className = "d";
 
-    Math.random();
+    var x = Math.random();
     d.style.marginLeft = 500 + "px";
 
     var b = document.getElementById("b");
