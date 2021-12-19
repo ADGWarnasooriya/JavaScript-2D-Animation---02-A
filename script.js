479ca3af715e431abd47d42b5620ffb99bc2636d
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-12-19 23:47:47 +0530

    random number need to het multiply

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function start() {
     var d = document.createElement("div");
     d.className = "d";
 
-    var x = Math.random();
+    var x = Math.random() * 1000;
     d.style.marginLeft = 500 + "px";
 
     var b = document.getElementById("b");
